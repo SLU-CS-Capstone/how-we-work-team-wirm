20ef1a7a90e49f613e6b51fb425d2582b40b74c6
Tianhao Wang <tianhao.wang@slu.edu>
2023-09-11 16:55:37 -0500

    4

--- src/generator.py
+++ src/generator.py
@@ -1,6 +1,10 @@
 from maze import Maze
-
+from fpdf import FPDF
+pdf = FPDF()
+pdf.add_page()
 maze = Maze(20)
 maze.generate_maze()
 maze.print()
+pdf.cell(200, 10, txt=maze.print, ln=True)
+pdf.output("example.pdf")
 print("Welcome to 2D maze")
