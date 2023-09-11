9720d6e96e8eed2a5c20555835bec2582769dd38
Tianhao Wang <tianhao.wang@slu.edu>
2023-09-11 17:06:18 -0500

    pdf

--- src/generator.py
+++ src/generator.py
@@ -2,9 +2,12 @@ from maze import Maze
 from fpdf import FPDF
 pdf = FPDF()
 pdf.add_page()
+
+pdf.set_font("Arial", size=12)
 maze = Maze(20)
 maze.generate_maze()
 maze.print()
-pdf.cell(200, 10, txt=maze.print, ln=True)
-pdf.output("example.pdf")
+maze_text = maze.print()
+pdf.multi_cell(0, 10, txt=maze_text)
+pdf.output("maze.pdf")
 print("Welcome to 2D maze")
